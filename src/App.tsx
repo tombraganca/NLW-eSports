8dcd8850fe7878f1aa7c97c8302ef1bf243b556e
Tom Santos <tom.santos@abas.online>
2022-09-22 20:28:43 -0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/main.css';
+import logoImg from './assets/eSports.svg'
+function App() {
+
+  return (
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center'>
+      <img src={logoImg}  />
+    </div>
+  )
+}
+
+export default App
